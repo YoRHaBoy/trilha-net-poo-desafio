505cd5c53505a261ce120d9607dc4a20a68f51ed
YoRHaBoy <brunoneves451@gmail.com>
2024-09-11 18:06:34 -0300

    Desafio Concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia n = new Nokia("(19)97150-9525", "G60", "001970-01-010000-0", 64);
+
+Iphone i = new Iphone("(19)97150-9522", "15", "001970-01-010000-1", 256);
+
+Console.WriteLine("Nokia: ");
+n.Ligar();
+n.ReceberLigacao();
+n.InstalarAplicativo("Nubank");
+
+Console.WriteLine("Iphone: ");
+i.Ligar();
+i.ReceberLigacao();
+i.InstalarAplicativo("Spotify");
